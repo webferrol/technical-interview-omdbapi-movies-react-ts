fd1e6f9b8bbc07c63b10024df76c977c998b88e8
webferrol <xurxo@webferrol.com>
2024-07-10 12:50:13 +0200

    Mocks create

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
 import './App.css'
+// import query from '../mocks/matrix-movie-search.json'
+import query from '../mocks/movie-not-found.json'
+// import query from '../mocks/invalid-id.json'
+// import query from '../mocks/invalid-apikey.json'
 
+export interface SearchEntity {
+  Title: string;
+  Year: string;
+  imdbID: string;
+  Type: string;
+  Poster: string;
+}
+
+export interface Query {
+  Search?: (SearchEntity)[] | null;
+  totalResults?: string;
+  Response: string;
+  Error?: string;
+}
+
+const { Response, totalResults, Search, Error }: Query = query
+// import.meta.env.VITE_API_KEY
+const isSuccess = Response === 'True'
 function App () {
+  if (!isSuccess) {
+    return (
+      <>
+        { Error}
+      </>
+    )
+  }
   return (
     <>
-     {import.meta.env.VITE_API_KEY}
+     { Boolean(totalResults) && JSON.stringify(Search) }
     </>
   )
 }
