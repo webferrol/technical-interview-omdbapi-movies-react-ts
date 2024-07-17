c471bd647baa6778b6696a2e4551a7bbab9ba37f
webFerrol <xurxo@webferrol.com>
2024-07-17 18:57:21 +0200

    updated getListMovies fix: problem with type return

--- src/helpers/getListMovies.tsx
+++ src/helpers/getListMovies.tsx
@@ -4,7 +4,11 @@ import { Query } from '../types'
 export const getListMovies = async (searchValue: string = 'croods') => {
   const res = await fetch(`${API_URL}?s=${searchValue}&apikey=${import.meta.env.VITE_API_KEY}`)
   if (!res.ok) {
-    throw new Error(`Not found: ${res.status} ${res.statusText}`)
+    return {
+      Search: null,
+      Response: 'False',
+      Error: `Not found (Apikey?): ${res.status} ${res.statusText}`
+    }
   }
   const data: Query = await res.json()
   return data
